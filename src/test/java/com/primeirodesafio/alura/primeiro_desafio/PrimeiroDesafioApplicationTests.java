1d8eb899c42e55284b015d91f7bd76991b4a36d6
Bazan <felipe_bazan3006@hotmail.com>
2025-10-23 16:56:58 -0300

    Commit inicial

---
+++ src/test/java/com/primeirodesafio/alura/primeiro_desafio/PrimeiroDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirodesafio.alura.primeiro_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
